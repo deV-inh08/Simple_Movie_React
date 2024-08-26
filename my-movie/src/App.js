ef8d2216e8846f81a79912a173ba607d72f1e254
deV-inh08 <vintd2004@gmail.com>
2024-08-26 20:52:14 +0700

    finaly setup

--- my-movie/src/App.js
+++ my-movie/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <Fragment>
+      <header className="header flex items-center justify-center gap-x-5 text-white mt-2">
+        <span className="text-primary">Home</span>
+        <span>Movies</span>
+      </header>
+    </Fragment>
+  )
 }
 
 export default App;
